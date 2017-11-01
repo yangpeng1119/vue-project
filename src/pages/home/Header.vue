12ab6aeb38da4baedd6cf5af72c8e9b5e2c15f39
ypp1119 <931819893@qq.com>
2017-11-01 08:46:17 +0800

    header change

---
+++ src/pages/home/Header.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+  	<div class="header">
+  		<div class="header-left"><</div>
+  		<div class="header-center">输入城市/景点/游玩主题</div>
+  		<div class="header-right">北京<span></span></div>
+  	</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'header'
+    }
+  }
+}
+</script>
+
+<style scoped>
+	.header{
+		display:flex;
+		box-sizing: border-box;
+		height:.88rem;
+		width:100%;
+		background:#00bcd4;
+	}
+	.header-left{
+		float:left;
+		width: .4rem;
+	    line-height: .88rem;
+	    padding: 0 .2rem;
+	    color: #fff;
+	    font-size: .36rem;
+	    text-align: left;
+	}
+	.header-center{
+		box-sizing: border-box;
+		float:left;
+		height: .6rem;
+		flex:1;
+	    margin: .14rem 0;
+	    background: #fff;
+	    line-height: .6rem; 
+	    float:left;
+	    padding:0 .2rem;
+	    border-radius: .1rem;
+	}
+	.header-right{
+		float:right ;
+		line-height: .88rem;
+		color:#fff;
+		margin:0 .3rem;
+	}
+	.header-right span{
+		float:right;
+		margin-top:.3rem;
+		margin-left:.1rem;
+		display:block;
+		width: 0;
+	    height: 0;
+	    border-left: .15rem solid transparent;
+	    border-right: .15rem solid transparent;
+	    border-top: .25rem solid #fff;
+	}
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+      <Index-header />
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    	
+    }
+  },
+  components: {
+  	'Index-header' :Header
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
